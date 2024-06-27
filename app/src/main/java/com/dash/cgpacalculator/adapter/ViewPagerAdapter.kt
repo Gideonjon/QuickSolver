c0cb22c752d26b72cca9dbf048d9504e5b956b8f
gideonjon <gideonjones63@gmail.com>
2024-06-27 18:37:15 +0100

    new-folders-added

--- app/src/main/java/com/dash/cgpacalculator/adapter/ViewPagerAdapter.kt
+++ app/src/main/java/com/dash/cgpacalculator/adapter/ViewPagerAdapter.kt
@@ -1,4 +1,4 @@
-package com.dash.cgpacalculator
+package com.dash.cgpacalculator.adapter
 
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
--- app/src/main/java/com/dash/cgpacalculator/onboarding/ViewPagerFragment.kt
+++ app/src/main/java/com/dash/cgpacalculator/onboarding/ViewPagerFragment.kt
@@ -5,7 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.dash.cgpacalculator.ViewPagerAdapter
+import com.dash.cgpacalculator.adapter.ViewPagerAdapter
 import com.dash.cgpacalculator.databinding.FragmentViewPagerBinding
 
 
